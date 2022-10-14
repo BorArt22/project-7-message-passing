81ab5a3d805fd21318389acaadb57e7f6f647b23
BorArt22 <borart22@gmail.com>
2022-10-14 10:15:20 +0200

    change name in deployment location

--- modules/library/udaconnect/__init__.py
+++
@@ -1,8 +0,0 @@
-from modules.library.udaconnect.models import Connection, Location, Person  # noqa
-from modules.library.udaconnect.schemas import ConnectionSchema, LocationSchema, PersonSchema  # noqa
-
-
-def register_routes(api, app, root="api"):
-    from modules.library.udaconnect.controllers import api as udaconnect_api
-
-    api.add_namespace(udaconnect_api, path=f"/{root}")
--- modules/library/udaconnect/models.py
+++ modules/library/udaconnect/models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from datetime import datetime
 
-from app import db  # noqa
+from library import db  # noqa
 from geoalchemy2 import Geometry
 from geoalchemy2.shape import to_shape
 from shapely.geometry.point import Point
