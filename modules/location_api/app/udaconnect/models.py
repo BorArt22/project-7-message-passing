45143121834c486415551b5cb02a3ccfa7770d45
BorArt22 <borart22@gmail.com>
2022-10-14 09:34:13 +0200

    clean module location

--- modules/location_api/app/udaconnect/models.py
+++ modules/location_api/app/udaconnect/models.py
@@ -12,15 +12,6 @@ from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 
 
-class Person(db.Model):
-    __tablename__ = "person"
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    company_name = Column(String, nullable=False)
-
-
 class Location(db.Model):
     __tablename__ = "location"
 
@@ -56,9 +47,3 @@ class Location(db.Model):
     def latitude(self) -> str:
         coord_text = self.wkt_shape
         return coord_text[coord_text.find("(") + 1 : coord_text.find(" ")]
-
-
-@dataclass
-class Connection:
-    location: Location
-    person: Person
