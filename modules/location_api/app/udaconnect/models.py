9b39c85beb0e312fe21b61881651cc4b9395b355
BorArt22 <borart22@gmail.com>
2022-10-14 11:51:32 +0200

    add person in models location-api

--- modules/location_api/app/udaconnect/models.py
+++ modules/location_api/app/udaconnect/models.py
@@ -11,6 +11,13 @@ from sqlalchemy import BigInteger, Column, Date, DateTime, ForeignKey, Integer, 
 from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.ext.hybrid import hybrid_property
 
+class Person(db.Model):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
 
 class Location(db.Model):
     __tablename__ = "location"
