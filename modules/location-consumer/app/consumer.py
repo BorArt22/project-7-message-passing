98957729a268a8193bda0e4822158026e1780e5f
BorArt22 <borart22@gmail.com>
2022-10-18 13:49:05 +0200

    test kafka

--- modules/location-consumer/app/consumer.py
+++ modules/location-consumer/app/consumer.py
@@ -1,4 +1,7 @@
 from kafka import KafkaConsumer
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 TOPIC_NAME = 'test'
 KAFKA_SERVER = 'kafka-service.kafka.svc.cluster.local:9092'
@@ -6,3 +9,4 @@ KAFKA_SERVER = 'kafka-service.kafka.svc.cluster.local:9092'
 consumer = KafkaConsumer(TOPIC_NAME,bootstrap_servers=KAFKA_SERVER)
 for message in consumer:
     print (message)
+    logging.info(f"Consumed message {message} into topic {TOPIC_NAME}")
--- modules/location-producer/app/producer.py
+++ modules/location-producer/app/producer.py
@@ -1,9 +1,28 @@
 from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import logging
 
-TOPIC_NAME = 'test'
-KAFKA_SERVER = 'kafka-service.kafka.svc.cluster.local:9092'
+logging.basicConfig(level=logging.INFO)
 
-producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
-producer.send(TOPIC_NAME, b'Test Message!!!')
-producer.flush()
+class Producer:
+    def __init__(self):
+        self._init_kafka_producer()
+
+    def _init_kafka_producer(self):
+        self.kafka_host = 'kafka-service.kafka.svc.cluster.local:9092'
+        self.kafka_topic = 'test'
+        self.producer = KafkaProducer(bootstrap_servers=self.kafka_host)
+
+    def publish_to_kafka(self, message):
+        try:
+            self.producer.send(self.kafka_topic, message)
+            self.producer.flush()
+        except KafkaError as ex:
+            logging.error(f"Exception {ex}")
+        else:
+            logging.info(f"Published message {message} into topic {self.kafka_topic}")
+
+if __name__ == "__main__":
+    producer = Producer()
+    producer.publish_to_kafka(b'Test Message!!!')
