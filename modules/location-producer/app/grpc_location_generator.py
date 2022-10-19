c5d7a91b6c5f49698d5471c09367199cc1f9ccb7
BorArt22 <borart22@gmail.com>
2022-10-19 13:21:13 +0200

    update  location-producer

--- modules/location-producer/app/grpc_location_generator.py
+++ modules/location-producer/app/grpc_location_generator.py
@@ -8,7 +8,7 @@ import location_pb2_grpc
 
 logging.basicConfig(level=logging.INFO)
 
-channel = grpc.insecure_channel("127.0.0.1:5021")
+channel = grpc.insecure_channel("localhost:30021")
 stub = location_pb2_grpc.LocationServiceStub(channel)
 
 fake = faker.Faker()
@@ -17,11 +17,14 @@ person_id = [1, 5, 6, 8, 9]
 def random_person():
     return random.choice(person_id)
 
-
 def random_float():
     return fake.pyfloat(min_value=-1, max_value=1)
 
-location_value = location_pb2.LocationMessage(person_id=random_person(), latitude=str(90*random_float()), longitude=str(180*random_float()))
+location_value = location_pb2.LocationMessage(
+    person_id=random_person(), 
+    latitude=str(90*random_float()), 
+    longitude=str(180*random_float())
+)
 
 response = stub.Create(location_value)
 logging.info(f"Response from gRPC server: {response}")
