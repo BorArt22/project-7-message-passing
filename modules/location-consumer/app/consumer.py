1d6a997fc95a7a91cc6d9367eaa58fc00cbebd2d
BorArt22 <borart22@gmail.com>
2022-10-19 09:20:41 +0200

    test kafka

--- modules/location-consumer/app/consumer.py
+++ modules/location-consumer/app/consumer.py
@@ -7,20 +7,15 @@ logging.basicConfig(level=logging.INFO)
 KAFKA_HOST = os.environ["KAFKA_HOST"]
 KAFKA_TOPIC = os.environ["KAFKA_TOPIC_TEST"]
 
-KAFKA_HOST = str(f'{KAFKA_HOST}')
-print("KAFKA HOST",KAFKA_HOST)
-
 class Consumer:
 
     def __init__(self):
         self._init_kafka_consumer()
 
     def _init_kafka_consumer(self):
-        self.kafka_host = KAFKA_HOST
-        self.kafka_topic = KAFKA_TOPIC
         self.consumer = KafkaConsumer(
-            self.kafka_topic,
-            bootstrap_servers=self.kafka_host,
+            KAFKA_TOPIC,
+            bootstrap_servers=[KAFKA_HOST],
         )
 
     def consume_from_kafka(self):
--- modules/location-producer/app/producer.py
+++ modules/location-producer/app/producer.py
@@ -6,29 +6,24 @@ from kafka.errors import KafkaError
 
 logging.basicConfig(level=logging.INFO)
 
-#KAFKA_HOST = os.environ["KAFKA_HOST"]
-#KAFKA_TOPIC = os.environ["KAFKA_TOPIC_TEST"]
-
-KAFKA_HOST = 'kafka-service.kafka.svc.cluster.local:9092'
-KAFKA_TOPIC = 'test'
+KAFKA_HOST = os.environ["KAFKA_HOST"]
+KAFKA_TOPIC = os.environ["KAFKA_TOPIC_TEST"]
 
 class Producer:
     def __init__(self):
         self._init_kafka_producer()
 
     def _init_kafka_producer(self):
-        self.kafka_host = KAFKA_HOST
-        self.kafka_topic = KAFKA_TOPIC
-        self.producer = KafkaProducer(bootstrap_servers=self.kafka_host)
+        self.producer = KafkaProducer(bootstrap_servers=[KAFKA_HOST])
 
     def publish_to_kafka(self, message):
         try:
-            self.producer.send(self.kafka_topic, message)
+            self.producer.send(KAFKA_TOPIC, message)
             self.producer.flush()
         except KafkaError as ex:
             logging.error(f"Exception {ex}")
         else:
-            logging.info(f"Published message {message} into topic {self.kafka_topic}")
+            logging.info(f"Published message {message} into topic {KAFKA_TOPIC}")
 
 if __name__ == "__main__":
     producer = Producer()
