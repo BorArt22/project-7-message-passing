4102558ece4903cadbb8f230022ced4cb574c504
BorArt22 <borart22@gmail.com>
2022-10-20 11:49:56 +0200

    add person api

---
+++ modules/person-api/app/udaconnect/models.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from app import db  # noqa
+from sqlalchemy import Column, Integer, String
+
+class Person(db.Model):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    company_name = Column(String, nullable=False)
