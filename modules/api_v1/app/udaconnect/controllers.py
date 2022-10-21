bf364a29c6df57c4aab7334ee8fcef5dd2e4aaf9
BorArt22 <borart22@gmail.com>
2022-10-21 21:14:07 +0200

    Update documents

--- modules/api_v1/app/udaconnect/controllers.py
+++ modules/api_v1/app/udaconnect/controllers.py
@@ -16,10 +16,6 @@ DATE_FORMAT = "%Y-%m-%d"
 
 api = Namespace("UdaConnect", description="Connections via geolocation.")  # noqa
 
-
-# TODO: This needs better exception handling
-
-
 @api.route("/locations")
 @api.route("/locations/<location_id>")
 @api.param("location_id", "Unique ID for a given Location", _in="query")
